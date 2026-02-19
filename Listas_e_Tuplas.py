077fa42e104e4808cf220b34c6196757657618a4
Andreia Dias <andreiadias2311@gmail.com>
2026-02-19 16:52:29 -0300

    Listas_e_Tuplas.py

---
+++ Listas_e_Tuplas.py
@@ -0,0 +1,8 @@
+# Crie uma lista contendo seis frutas de sua escolha. Depois de ter a lista pronta, converta essa lista em uma tupla. Por fim, exiba o conteúdo da tupla resultante para verificar as frutas que foram armazenadas.
+
+lista_frutas = ['maça','morango','manga','melao','melancia','banana']
+
+#convertendo
+tupla_lista_frutas = tuple(lista_frutas)
+
+print(tupla_lista_frutas)
